9a7496e2d10b9b1defd13ceada8a023e3ec85660
Habib Kadiri <kadirit@hotmail.com>
2020-07-05 18:14:59 +0200

    changed dev.sh file

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-#python -c "import time; time.sleep(3)" # Wait for postgres to start up
 python manage.py makemigrations
 python manage.py migrate
-python manage.py runserver 0.0.0.0:8000 # "runserver" is good enough for local environment
+python manage.py runserver 0.0.0.0:8000
