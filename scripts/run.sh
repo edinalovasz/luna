2ad49b009df6cf00ed98e0dae465625fefe25d64
Habib Kadiri <kadirit@hotmail.com>
2020-07-05 20:12:14 +0200

    added luna_project insteade of project in gunicorn command in run.sh script

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,4 +2,4 @@
 python manage.py migrate
 rm -rf /frontend/build/* && cp -r /frontend_tmp/build/* /frontend/build
 python manage.py collectstatic --no-input
-gunicorn -w 4 -b 0.0.0.0:8000 project.wsgi:application
+gunicorn -w 4 -b 0.0.0.0:8000 luna_project.wsgi:application
