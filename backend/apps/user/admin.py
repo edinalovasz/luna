8e35b3ff5d52bfd052208e74d15ff6277b0d22f4
Habib Kadiri <kadirit@hotmail.com>
2020-07-05 12:42:16 +0200

    added user model

---
+++ backend/apps/user/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+User = get_user_model()
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2'),
+        }),
+    )
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Social', {'fields': ('location', 'about_me', 'things_user_likes', 'phone_number')}),
+        ('Files', {'fields': ('avatar', 'banner')}),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    readonly_fields = ('date_joined', 'last_login')
+    list_display = ('pk', 'username', 'email', 'is_staff')
+    ordering = ('-email',)
---
+++ backend/apps/user/models.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import AbstractUser
+from django.contrib.postgres.fields import ArrayField
+from django.db import models
+
+
+# Create your models here.
+
+class User(AbstractUser):
+    # Field used for authentication in Django admin
+    USERNAME_FIELD = 'email'
+    # Extra fields needed when using createsuperuser (by default USERNAME_FIELD and passwords
+    REQUIRED_FIELDS = ['username']
+
+    email = models.EmailField(unique=True, default='example@email.com')
+
+    location = models.CharField(blank=True, max_length=1000)
+
+    about_me = models.CharField(blank=True, max_length=2000)
+
+    avatar = models.ImageField(null=True, blank=True)
+
+    phone_number = models.CharField(blank=True, max_length=15)
+
+    def __str__(self):
+        return f'User ID: {self.id} Name: {self.first_name}'
