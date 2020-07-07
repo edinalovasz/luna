8536c674b86ecce8b471ca7a334d67450d71645a
Habib Kadiri <kadirit@hotmail.com>
2020-07-07 21:25:08 +0200

    fixed top rated restaurants

--- backend/apps/general/views.py
+++ backend/apps/general/views.py
@@ -1,16 +1,20 @@
-from django.db.models import Avg, Count
+from django.db.models import Avg, Count, Subquery
+from django.db.models.functions import Coalesce
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from rest_framework.fields import DecimalField
 from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 
 from apps.registrations.permissions import CanBeAnonymous
+from apps.restaurantreviews.models import RestaurantReview
 from apps.restaurants.models import Restaurant
 from apps.restaurants.serializer import RestaurantSerializer
 
 
-class ListTopFourRestaurants(ListAPIView):
+class ListTopFourRestaurants(generics.ListAPIView):
     """
     GET:
     List the top 4 rated reviews.
@@ -19,11 +23,6 @@ class ListTopFourRestaurants(ListAPIView):
     serializer_class = RestaurantSerializer
 
     def list(self, request, *args, **kwargs):
-
-        queryset = Restaurant.objects.annotate(count=Count('restaurant_reviews')).order_by('-count').all()[:4]
-
-        # queryset = Restaurant.objects.filter(
-        #     restaurant_reviews__rating=Restaurant.objects.order_by('restaurant_reviews__rating')[9].restaurant_reviews
-        # )
+        queryset = sorted(Restaurant.objects.all(), key=lambda m: (m.avg_rating, m.no_of_ratings), reverse=True)[:4]
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
