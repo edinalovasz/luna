09777cd512ea7c2788d68eb5d233c007cf2f39ac
erickotd <erickotd@gmail.com>
2020-07-07 09:00:29 +0200

    add verification page

--- frontend/src/components/Credentials/Verification/index.js
+++ frontend/src/components/Credentials/Verification/index.js
@@ -3,16 +3,15 @@ import {BaseInput} from "../../../style/GlobalInputs";
 import {BigButton} from "../../../style/GlobalButtons";
 import styled from "styled-components";
 import {rem} from "polished";
-import {BigTitleHr, MainTitle, TitleHr} from "../../../style/GlobalTitles";
+import {MainTitle, TitleHr} from "../../../style/GlobalTitles";
 import {TitleContainer} from "../../../style/GlobalWrappers";
 
 const VerificationWrapper = styled.div`
+    background: #F2F2F2;
     height: 100vh;
-    display: flex;
     display: flex;
     flex-direction: column;
     align-items: center;
-    border: solid red;
     justify-content: center;
 `
 
@@ -20,20 +19,37 @@ const VerificationForm = styled.form`
     display: flex;
     flex-direction: column;
     align-items: center;
-    border: solid red;
+    margin-bottom: 200px;
 `
 
 const Line = styled(TitleHr)`
-  border: 3px solid #e47d31;
-  width: ${rem("170px")};
+  border: 2px solid #e47d31;
+  width: ${rem("120px")};
+  margin: 8px;
 `;
 
+const InputsContainers = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr ;
+  grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr ;
+  gap: 0px 30px;
+  grid-template-areas: ". ." ". ." ". .";
+  margin: 100px;
+    div {
+      border: solid red;
+      margin: 10px;
+    }
+`
 const VerInput = styled(BaseInput)`
-    
+  width: ${rem("500px")};
+  height: ${rem("55px")};
+  font-size: ${rem("20px")};
+  background: #FFFFFF
 `;
 
 
 const Verification = (props) => {
+
     return (
         <>
             <VerificationWrapper>
@@ -42,7 +58,44 @@ const Verification = (props) => {
                             <MainTitle>Verification</MainTitle>
                             <Line></Line>
                         </TitleContainer>
-                        <VerInput></VerInput>
+                        <InputsContainers>
+                            <div></div>
+                            <div></div>
+                            <VerInput
+                                type='text'
+                                placeholder='Email address'
+                                name='email'
+                            />
+                            <VerInput
+                                type='text'
+                                placeholder='Validation code'
+                                name='email'
+                            />
+                            <div></div>
+                            <div></div>
+                            <VerInput
+                                type='text'
+                                placeholder='Username'
+                                name='email'
+                            />
+                            <VerInput
+                                type='text'
+                                placeholder='Location'
+                                name='email'
+                            />
+                            <div></div>
+                            <div></div>
+                            <VerInput
+                                type='text'
+                                placeholder='Password'
+                                name='email'
+                            />
+                            <VerInput
+                                type='text'
+                                placeholder='Password repeat'
+                                name='email'
+                            />
+                        </InputsContainers>
                         <BigButton>Finish Registration</BigButton>
                 </VerificationForm>
             </VerificationWrapper>
