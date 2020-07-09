a9766ca2047979a65330c3666259ade8a4ef38b4
Habib Kadiri <kadirit@hotmail.com>
2020-07-09 21:17:11 +0200

    reset runner

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,6 @@
 
 // const baseUrl = 'http://localhost:8000/api/';  ////
-const baseUrl = 'https://scorpio-luna.propulsion-learn.ch/api/';  ///
+const baseUrl = 'https://scorpio-luna.propulsion-learn.ch/api/';
 
 export const defaultUserProfileBanner = 'https://via.placeholder.com/1200x300';
 
