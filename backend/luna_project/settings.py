eac9ddb0cfac2dabf8b31874f52fe86893d81820
Edina Lovasz <63780895+edinalovasz@users.noreply.github.com>
2020-08-30 18:50:02 +0200

    Update settings.py

--- backend/luna_project/settings.py
+++ backend/luna_project/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ggq+2-qy23&_w@886(_l%h&^(_df98&%@vfe9wg2ka)3$k8%-r'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
